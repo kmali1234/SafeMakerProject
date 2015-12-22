52156e3420eb7e9066dbec1ce5dae3b45d5c34bf
davuluris <sudhir.davuluri@sysnetglobalsolutions.com>
2015-12-22 12:01:46 +0530

    comit

--- test/integration/com/sysnet/pageobjects/PersonaliseValidation.java
+++ test/integration/com/sysnet/pageobjects/PersonaliseValidation.java
@@ -4,12 +4,9 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import com.mysql.jdbc.Driver;
 
 public class PersonaliseValidation {
-	
+
 	private By usernameFieldLocator;
 	private By emailLocator;
 	private By passwordLocator;
@@ -23,56 +20,67 @@ public class PersonaliseValidation {
 	private By emailValidationMsg;
 	private Properties props;
 
-	
-	
-
 	public PersonaliseValidation(WebDriver driver, Properties props) {
 		// TODO Auto-generated constructor stub
-		this.driver=driver;
+		this.driver = driver;
 		this.props = props;
-		this.usernameFieldLocator=By.cssSelector(props.getProperty("personalise.textfield.username.css"));
-		//this.conformUsernameLocator=By.id(props.getProperty("personalise.textfield.confirmUsername.css"));
-		this.passwordLocator=By.cssSelector(props.getProperty("personalise.textfield.password.css"));
-		this.conformPasswordLocator=By.cssSelector(props.getProperty("personalise.textfield.confirmPassword.css"));
-		this.emailLocator=By.cssSelector(props.getProperty("personalise.textfield.email.css"));
-		this.conformEmailLocators= By.cssSelector(props.getProperty("personalise.textfield.confirmEmail.css"));
-		this.submitButtonLocator= By.cssSelector(props.getProperty("personalise.button.submit.css"));
-		this.sucessValidation= By.cssSelector(props.getProperty("personalise.successValidation.username.css"));
-		this.minLengthUsername=By.cssSelector(props.getProperty("personalise.minlength.usernam.css"));
-		this.emailValidationMsg=By.cssSelector(props.getProperty("personalise.validationmsg.email.css"));
+		this.usernameFieldLocator = By.cssSelector(props
+				.getProperty("personalise.textfield.username.css"));
+		// this.conformUsernameLocator=By.id(props.getProperty("personalise.textfield.confirmUsername.css"));
+		this.passwordLocator = By.cssSelector(props
+				.getProperty("personalise.textfield.password.css"));
+		this.conformPasswordLocator = By.cssSelector(props
+				.getProperty("personalise.textfield.confirmPassword.css"));
+		this.emailLocator = By.cssSelector(props
+				.getProperty("personalise.textfield.email.css"));
+		this.conformEmailLocators = By.cssSelector(props
+				.getProperty("personalise.textfield.confirmEmail.css"));
+		this.submitButtonLocator = By.cssSelector(props
+				.getProperty("personalise.button.submit.css"));
+		this.sucessValidation = By.cssSelector(props
+				.getProperty("personalise.successValidation.username.css"));
+		this.minLengthUsername = By.cssSelector(props
+				.getProperty("personalise.minlength.usernam.css"));
+		this.emailValidationMsg = By.cssSelector(props
+				.getProperty("personalise.validationmsg.email.css"));
 	}
 
+	public PersonaliseValidation usernamevalidation() {
 
-
-	public PersonaliseValidation usernamevalidation(){
-		
-		
-		
-		do{
-			driver.findElement(usernameFieldLocator).sendKeys(Integer.toString(unLength));
+		do {
+			driver.findElement(usernameFieldLocator).sendKeys(
+					Integer.toString(unLength));
 			driver.findElement(submitButtonLocator).click();
 			++unLength;
-		}while(driver.findElement(minLengthUsername).getText().equals("Minimum characters requirement is not reached!"));
-			
-		System.out.println("Minmum length of username under personal page is "+unLength);
+		} while (driver.findElement(minLengthUsername).getText()
+				.equals("Minimum characters requirement is not reached!"));
+
+		System.out.println("Minmum length of username under personal page is "
+				+ unLength);
 		System.out.println();
-		
+
 		return this;
-		
+
 	}
-	
-	public PersonaliseValidation blankEmail(){
+
+	public PersonaliseValidation blankEmail() {
 		driver.findElement(emailLocator).clear();
 		driver.findElement(submitButtonLocator).click();
-		String actEmailValidMsg= driver.findElement(emailValidationMsg).getText();
-		String expEmailValidMsg= props.getProperty("Personalise.validationmsg.email.text");
-		if(actEmailValidMsg.equals(expEmailValidMsg)){
-			System.out.println("Actual matches with expected, expected emial validation message is "+ expEmailValidMsg);
-		}else{
-			System.out.println("Actuall Message: "+actEmailValidMsg+" does not match with expected message: "+expEmailValidMsg);
+		String actEmailValidMsg = driver.findElement(emailValidationMsg)
+				.getText();
+		String expEmailValidMsg = props
+				.getProperty("Personalise.validationmsg.email.text");
+		if (actEmailValidMsg.equals(expEmailValidMsg)) {
+			System.out
+					.println("Actual matches with expected, expected emial validation message is "
+							+ expEmailValidMsg);
+		} else {
+			System.out.println("Actuall Message: " + actEmailValidMsg
+					+ " does not match with expected message: "
+					+ expEmailValidMsg);
 		}
 		return this;
-		
+
 	}
 
 }
--- test/integration/com/sysnet/safemaker/DoPersonalise.java
+++ test/integration/com/sysnet/safemaker/DoPersonalise.java
@@ -1,6 +1,5 @@
 package com.sysnet.safemaker;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
@@ -17,108 +16,89 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import sun.util.logging.resources.logging;
-
-import com.sysnet.helper.Assertions;
 import com.sysnet.helper.SeleniumHelper;
 import com.sysnet.pageobjects.LoginPage;
 import com.sysnet.pageobjects.LogoutPage;
 import com.sysnet.pageobjects.PersonalisePage;
 
+//import com.sun.java.util.jar.pack.Package.File;
 
+public class DoPersonalise {
 
-	//import com.sun.java.util.jar.pack.Package.File;
+	private Properties clientProps;
+	private String loginpageUrlSufix;
+	private WebDriver driver;
+	private String username;
+	private String propertyfilepath;
+	private String password;
+	private String url;
+	private Object baseUrl;
 
-		public class DoPersonalise {
-			
-			private Properties clientProps;
-			private String loginpageUrlSufix;
-			private WebDriver driver;
-			private String username;
-			private String propertyfilepath;
-			private String password;
-			private String url;
-			private Object baseUrl;
-			
-			private Sheet merchantFile;
-			private int count;
-			private Row midRow;
-			private String expUsername;
-		
-			private final Logger log = Logger.getLogger(DoLogin.class.getClass());
-			
-			@Before
-			public void SetUp() throws Exception
-			{
-				
-				
-				propertyfilepath="test/integration/aibms.properties";
-				clientProps = new Properties();
-				FileInputStream locatorStream = new FileInputStream(propertyfilepath);
-				clientProps.load(locatorStream);
-				baseUrl=clientProps.getProperty("baseUrl");
-				loginpageUrlSufix=clientProps.getProperty("login.url.suffix");
-				url = baseUrl+loginpageUrlSufix;
-				
-		
-				merchantFile = SeleniumHelper.readExcelFile("test/integration/TestAccs.xlsx", "Status");
-				count = merchantFile.getLastRowNum();
-		        
-				
-				
-			}
-			
-			
-			@Test
-			public void getPersonalise() throws Exception
-			{
-				
-				driver = new FirefoxDriver();
-				driver.manage().window().maximize();
-				driver.get(url);
-				for(int i=1; i<=count; i++){
-					midRow = merchantFile.getRow(i);
-					username = midRow.getCell(0).toString();
-					password = "Sysnet12";
-					expUsername = "test"+username;
-					
-				
-				Thread.sleep(500);	
-				LoginPage lp = new LoginPage(driver, clientProps);
-				lp.LoginUser(username, password);
-				log.info(username+" logged in sucessufully");
-				PersonalisePage pp = new PersonalisePage(driver, clientProps);
-				pp.personaliseMerchant(expUsername);
-				log.info(expUsername+" personalised sucessfully");
-				
-				// take the screenshot at the end of every test
-		        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		        // now save the screenshto to a file some place
-		        FileUtils.copyFile(scrFile, new java.io.File("ComplianceProject/Screenshots"));
-//		        Assertions pers = new Assertions(driver, clientProps);
-//		        pers.assertTitle("Personalise");
-		        Thread.sleep(3000);
-		        LogoutPage lop = new LogoutPage(driver, clientProps);
-		        lop.userLogout();
-		        // driver.switchTo().alert().accept();
-		        
-		        
-				}
-		        
-		        
-		        
-			}
+	private Sheet merchantFile;
+	private int count;
+	private Row midRow;
+	private String expUsername;
 
-			
-			@After
-			public void Quit()
-			{
-				 driver.close();
-				 
-			}
-			
-			
+	private final Logger log = Logger.getLogger(DoLogin.class.getClass());
 
+	@Before
+	public void SetUp() throws Exception {
+
+		propertyfilepath = "test/integration/aibms.properties";
+		clientProps = new Properties();
+		FileInputStream locatorStream = new FileInputStream(propertyfilepath);
+		clientProps.load(locatorStream);
+		baseUrl = clientProps.getProperty("baseUrl");
+		loginpageUrlSufix = clientProps.getProperty("login.url.suffix");
+		url = baseUrl + loginpageUrlSufix;
+
+		merchantFile = SeleniumHelper.readExcelFile(
+				"test/integration/TestAccs.xlsx", "Status");
+		count = merchantFile.getLastRowNum();
 
 	}
 
+	@Test
+	public void getPersonalise() throws Exception {
+
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		for (int i = 1; i <= count; i++) {
+			midRow = merchantFile.getRow(i);
+			username = midRow.getCell(0).toString();
+			password = "Sysnet12";
+			expUsername = "test" + username;
+
+			Thread.sleep(500);
+			LoginPage lp = new LoginPage(driver, clientProps);
+			lp.LoginUser(username, password);
+			log.info(username + " logged in sucessufully");
+			PersonalisePage pp = new PersonalisePage(driver, clientProps);
+			pp.personaliseMerchant(expUsername);
+			log.info(expUsername + " personalised sucessfully");
+
+			// take the screenshot at the end of every test
+			File scrFile = ((TakesScreenshot) driver)
+					.getScreenshotAs(OutputType.FILE);
+			// now save the screenshto to a file some place
+			FileUtils.copyFile(scrFile, new java.io.File(
+					"ComplianceProject/Screenshots"));
+			// Assertions pers = new Assertions(driver, clientProps);
+			// pers.assertTitle("Personalise");
+			Thread.sleep(3000);
+			LogoutPage lop = new LogoutPage(driver, clientProps);
+			lop.userLogout();
+			// driver.switchTo().alert().accept();
+
+		}
+
+	}
+
+	@After
+	public void Quit() {
+		driver.close();
+
+	}
+
+}
